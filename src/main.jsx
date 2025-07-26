05f14b07446e1dce83d1ff93b74950899232f953
Krizia Bianca <krizia.dev@gmail.com>
2025-07-26 13:34:03 +0800

    Rebuild

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode, React } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+
+createRoot(document.getElementById('body-lg')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
