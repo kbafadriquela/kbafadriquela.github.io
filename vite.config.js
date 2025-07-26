e84e0732c093efa4ae421ff3d53b397cb95520b1
Krizia Bianca <krizia.dev@gmail.com>
2025-07-26 14:13:57 +0800

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-// import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default {
+export default defineConfig({
   plugins: [react()],
-  base: '/'
-}
+  base: './', // relative paths
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
+});
